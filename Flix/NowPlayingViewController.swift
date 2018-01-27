91351c0fbf6f2de88257611926352ec439d78ce0
Pranaya Adhikari <pranaya.adhikari@bison.howard.edu>
2018-01-27 16:41:54 -0500

    complete user story: user sees loading state

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -15,6 +15,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     var movies:[[String:Any]] = []
     var refreshControl: UIRefreshControl!
     
@@ -35,6 +36,8 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     }
     
     func fetchMovies(){
+        self.activityIndicator.startAnimating()
+
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy:.reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -49,6 +52,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 self.movies = movies
                 self.tableView.reloadData()
                 self.refreshControl.endRefreshing()
+                self.activityIndicator.stopAnimating()
                 
             }
         }
