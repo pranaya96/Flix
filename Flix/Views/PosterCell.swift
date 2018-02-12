b1f5cb0196db6057fc8830c4733cc58028f862f8
Pranaya Adhikari <pranaya.adhikari@bison.howard.edu>
2018-02-12 00:23:51 -0500

    collection view setup

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Pranaya Adhikari on 2/11/18.
+//  Copyright © 2018 Pranaya Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
